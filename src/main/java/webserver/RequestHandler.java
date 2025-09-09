d47219957dc1d7b4de74551d541fe1939010dea9
tkdtn4657 <tkdtn101226@gmail.com>
2025-09-09 17:21:17 +0900

    feat: 사용자 요청에 따른 경로 데이터 반환

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,20 +11,38 @@ import org.slf4j.LoggerFactory;
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
 
     public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+        log.debug("New Client Connect! Connected IP : {}, Port : {}",
+                connection.getInetAddress(),
+                connection.getPort()
+        );
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+//            log.debug("inputStream : {} ");
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello Java World WebHookTest".getBytes();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder requestData = new StringBuilder();
+
+            String line;
+            //first Line Request
+            String[] requestFirstLines = reader.readLine().split(" ");
+            final String requestMethod = requestFirstLines[0];
+            final String requestUri = requestFirstLines[1];
+            final String requestProtocol = requestFirstLines[2];
+
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                requestData.append(line).append("\n");
+            }
+            log.info("RequestData\n{}", requestData);
+
+            byte[] body = Files.readAllBytes(new File("./webapp"+requestUri).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
@@ -36,6 +53,17 @@ public class RequestHandler extends Thread {
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response404Header(DataOutputStream dos, int lengthOfBodyContent){
+        try {
+            dos.writeBytes("HTTP/1.1 404 ERROR \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
